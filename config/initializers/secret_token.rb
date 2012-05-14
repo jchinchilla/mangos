a1a326bd2441d7bf4e61de501422ffc293fff412
Jonathan Chinchilla <chinchilla.jon@gmail.com>
2012-05-14 12:09:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mangos::Application.config.secret_token = '5e487dff889277ad8d595a772e6969a86b4b431092c1d87dff0b35361fb8dd17116bf3b4e8de0b51f3c2ba58f4257691804f5d3056577dfe0474183599c230b2'
